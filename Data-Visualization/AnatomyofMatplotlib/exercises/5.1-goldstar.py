5300b203cdbe87291f32b6da17a5dd340d695d4c
Casper Guo <casperg@umich.edu>
2024-08-09 14:17:55 -0700

    Remove data visualization tutorials.
    Repositioned to the dedicated tutorials repo

--- Data-Visualization/AnatomyofMatplotlib/exercises/5.1-goldstar.py
+++
@@ -1,9 +0,0 @@
-from matplotlib.collections import StarPolygonCollection
-
-fig, ax = plt.subplots(1, 1)
-
-collection = StarPolygonCollection(5,
-                                   offsets=[(0.5, 0.5)],
-                                   transOffset=ax.transData)
-ax.add_collection(collection)
-plt.show()
